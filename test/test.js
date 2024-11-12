c6eb9e5d82de5faf74f9d94d604279bddfc8aecf
Spenser Buck <117900923+Spenser-Buck@users.noreply.github.com>
2024-11-12 03:24:09 +0000

    AddedUndefined

--- test/test.js
+++ test/test.js
@@ -1,4 +1,3 @@
-const { hasUncaughtExceptionCaptureCallback } = require('process');
 var main = require('../index');
 var assert = require('assert');
 describe('Regular factorial value', function() {
@@ -11,5 +10,12 @@ describe('First two terms', function() {
   it('should return 1 for first two terms', function() {
       assert.strictEqual(main.factorial(0), 1);
       assert.strictEqual(main.factorial(1), 1);
+    });
+});
+
+describe('Undefined', function() {
+  it('should return "undefined" for negative numbers', function() {
+      assert.strictEqual(main.factorial(-1), undefined);
+      assert.strictEqual(main.factorial(-100), undefined);
     });
 });
